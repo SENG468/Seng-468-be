83d460400c5877d34990462bedd2d7d597a59bc1
eetar1 <ekwan14@gmail.com>
2021-02-12 19:03:51 -0800

    fix sells

--- src/main/java/com/daytrade/stocktrade/Models/Quote.java
+++ src/main/java/com/daytrade/stocktrade/Models/Quote.java
@@ -2,8 +2,10 @@ package com.daytrade.stocktrade.Models;
 
 import java.time.Instant;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Quote {
   public Quote(
       String id,
