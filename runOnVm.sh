e24794dfe6ac820720113d278ff9a6c9c92290bd
eetar1 <ekwan14@gmail.com>
2021-03-21 19:06:34 -0700

    new envs

--- runOnVm.sh
+++ runOnVm.sh
@@ -1,2 +1,2 @@
 docker pull eetar1/stocktrade
-docker run --network="host" -p 8080:8080 -e REDIS_HOST=127.0.0.1 -e MONGO_HOST=127.0.0.1 -e SPRING_PROFILES_ACTIVE=prod -e JWT_SECRET=super-secret eetar1/stocktrade
+docker run --network="host" -p 8080:8080 -e SERVER_NAME=NAME -e DEBUG=true -e REDIS_HOST=127.0.0.1 -e MONGO_HOST=127.0.0.1 -e SPRING_PROFILES_ACTIVE=prod -e JWT_SECRET=super-secret eetar1/stocktrade
